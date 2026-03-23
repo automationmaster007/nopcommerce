1446945d0367f256c2272d9895c0cd08b1fff0a4
Kashish <kashishyadav0106@gmail.com>
2026-03-23 15:32:13 +0530

    0.03 : Check

--- tests/program2.spec.js
+++ tests/program2.spec.js
@@ -4,4 +4,5 @@ test("Program for Google" , async ({browser}) => {
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto("https://www.google.com");
+    
 });
