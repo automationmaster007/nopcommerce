b28a768709a0c78c4595b86a1ed86fae058ebbaf
Kashish <kashishyadav0106@gmail.com>
2026-03-23 17:50:43 +0530

    COMMIT

--- tests/program2.spec.js
+++ tests/program2.spec.js
@@ -3,6 +3,6 @@ const { test } = require("@playwright/test");
 test("Program for Google" , async ({browser}) => {
     const context = await browser.newContext();
     const page = await context.newPage();
-    await page.goto("https://www.google.com");
-    
+    await page.goto("https://demo.prestashop.com/#/en/front");
+
 });
